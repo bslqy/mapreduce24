481b2f5fd126e02574542ef97ac25a0c2fbb4dde
LiaoG <LiaoG@AU00065>
2019-12-13 14:51:46 +1100

    Hadoop Day06 - 13.zookeeper的java客户端api的监听功能代码示范

---
+++ src/main/java/cn/edu360/zk/demo/ZokeeperWatchDemo.java
@@ -0,0 +1,59 @@
+package cn.edu360.zk.demo;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Before;
+
+import java.awt.*;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class ZokeeperWatchDemo {
+
+    ZooKeeper zk = null;
+    @Before
+    public void init() throws Exception {
+
+        zk =  new ZooKeeper("hdp-01:2181,hdp-02:2181,hdp-03:2181",2000,new Watcher() {
+
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                if (watchedEvent.getState() == Event.KeeperState.SyncConnected && watchedEvent.getType() == Event.EventType.NodeDataChanged) {
+                    // 只监听一次
+                    System.out.println(watchedEvent.getPath());
+                    System.out.println(watchedEvent.getType());
+
+                    // Handling
+                    System.out.println("change");
+
+                    //实现长期监听
+                    try {
+                        zk.getData("/mygirl", true, null);
+                    } catch (KeeperException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                else if (watchedEvent.getState() == Event.KeeperState.SyncConnected && watchedEvent.getType() == Event.EventType.NodeChildrenChanged){
+                    System.out.println("Child node change");
+                }
+            }
+        });
+    }
+
+    public void testGetWatach() throws KeeperException, InterruptedException, UnsupportedEncodingException {
+        byte[] data = zk.getData("/mygirls/",true,null);
+
+        //监听节点的子节点
+        List<String> children = zk.getChildren("/mygirls", true, null);
+
+        System.out.println(new String(data,"UTF-8"));
+
+        Thread.sleep(Long.MAX_VALUE);
+    }
+
+
+}
