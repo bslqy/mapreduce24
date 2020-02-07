0a6f8693f32161f2fd56776b7c7b4db949cbddaa
bslqy <853185345@qq.com>
2020-02-07 18:03:21 +1100

    Day3 07.媒体报表实现---改造---将redis作为字典库 （mapPartition + Jedis 需要ListBuffer保存数据

--- src/main/Scala/cn/dmp/report/AppAnalysisRedis.scala
+++ src/main/Scala/cn/dmp/report/AppAnalysisRedis.scala
@@ -27,7 +27,7 @@ object AppAnalysisRedis {
     sparkConf.setAppName(s"${this.getClass.getSimpleName}")
     sparkConf.setMaster("local[*]")
     // RDD 序列化到磁盘 worker与worker之间的数据传输
-    sparkConf.set("spark.serializer", "org.apache.spark.serializer.KyroSerializer")
+    sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
     val sc = new SparkContext(sparkConf)
     // 读取App字典
